908ad7a06d353d351121f48e071fc06d37bf10c1
Anna Beatriz <69550398+annabefsm@users.noreply.github.com>
2021-04-17 19:05:31 -0300

    Add files via upload
    Criação de primeiras classes do Model

---
+++ jobsgoias/src/main/java/jobsgoias/model/Endereco.java
@@ -0,0 +1,5 @@
+package jobsgoias.model;
+
+public class Endereco {
+
+}
---
+++ jobsgoias/src/main/java/jobsgoias/model/Estudante.java
@@ -0,0 +1,94 @@
+package jobsgoias.model;
+
+import java.time.LocalDate;
+
+public class Estudante {
+	
+	private long id;
+	private String nome;
+	private String cpf;
+	private LocalDate dataNascimento;
+	private String estadoCivil;
+	private Endereco endereco;
+	private Telefone telefone;
+	private Curriculo dadosCurriculo;
+	private String email;
+	private String escolaridade;
+	private Usuario usuario;
+	
+	public Estudante(long id) {
+		super();
+		this.id = id;
+	}
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public Telefone getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(Telefone telefone) {
+		this.telefone = telefone;
+	}
+	public Curriculo getDadosCurriculo() {
+		return dadosCurriculo;
+	}
+	public void setDadosCurriculo(Curriculo dadosCurriculo) {
+		this.dadosCurriculo = dadosCurriculo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEscolaridade() {
+		return escolaridade;
+	}
+	public void setEscolaridade(String escolaridade) {
+		this.escolaridade = escolaridade;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+
+}
